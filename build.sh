27c10b78cbaae6f8377d805d9667e51ef2b41439
sirisurab <siri.surab@gmail.com>
2018-12-13 16:05:17 -0600

    spacy-model

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git push --repo git://github.com/sirisurab/dupquest.git origin master
+sudo git push --repo git://github.com/sirisurab/dupquest.git origin master
 
 docker build -t sirisurab/dq-app --no-cache git@github.com:sirisurab/dupquest.git
 
