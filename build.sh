93564838baeac7aa2c3b2ce3f1f6210c63333d3a
sirisurab <siri.surab@gmail.com>
2019-03-26 09:27:37 -0500

    refactor

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git push --repo git://github.com/sirisurab/dupquest.git origin master
-
-docker build -t sirisurab/dq-app --no-cache git@github.com:sirisurab/dupquest.git
-
-docker login -u sirisurab -p everestK2
-
-docker push sirisurab/dq-app
